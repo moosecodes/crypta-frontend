2ad022973075ccc38025a89573db717beb686227
Moose <moosecodes@gmail.com>
2021-07-24 09:26:46 -0700

    Add Encryption view and enable bootstrapvue

---
+++ src/views/Encryption.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="encryption">
+    <encryption-boxes />
+    <ciphers />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import EncryptionBoxes from '@/components/EncryptionBoxes.vue';
+import Ciphers from '@/components/Ciphers.vue';
+
+@Component({
+  components: {
+    EncryptionBoxes,
+    Ciphers,
+  },
+})
+export default class Encryption extends Vue {}
+</script>
